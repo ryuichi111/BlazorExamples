3995876c961fb6fac99335ea358306789e3af7af
ryuichi111 <daigo@clearboxtechnology.net>
2019-05-04 15:54:19 +0900

    add web_api_blazor

---
+++ web_api_blazor/Models/WeatherResult.cs
@@ -0,0 +1,88 @@
+using System;
+using System.Collections.Generic;
+
+namespace web_api_blazor.Models {
+	public partial class WeatherResult
+    {
+        public long Cod { get; set; }
+        public double Message { get; set; }
+        public long Cnt { get; set; }
+        public List<WeatherSummary> List { get; set; }
+        public City City { get; set; }
+    }
+
+    public partial class City
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public Coord Coord { get; set; }
+        public string Country { get; set; }
+        public long Population { get; set; }
+    }
+
+    public partial class Coord
+    {
+        public double Lat { get; set; }
+        public double Lon { get; set; }
+    }
+
+    public partial class WeatherSummary
+    {
+        public long Dt { get; set; }
+        public MainClass Main { get; set; }
+        public List<Weather> Weather { get; set; }
+        public Clouds Clouds { get; set; }
+        public Wind Wind { get; set; }
+        public Sys Sys { get; set; }
+        public DateTime Dt_Txt { get; set; }
+        public DateTime DtJp { get { return this.Dt_Txt.AddHours(9); } }
+        public Rain Rain { get; set; }
+    }
+
+    public partial class Clouds
+    {
+        public long All { get; set; }
+    }
+
+    public partial class MainClass
+    {
+        public double Temp { get; set; }
+        public double TempMin { get; set; }
+        public double TempMax { get; set; }
+        public double Pressure { get; set; }
+        public double SeaLevel { get; set; }
+        public double GrndLevel { get; set; }
+        public long Humidity { get; set; }
+        public double TempKf { get; set; }
+    }
+
+    public partial class Rain
+    {
+        public double The3H { get; set; }
+    }
+
+    public partial class Sys
+    {
+        public string Pod { get; set; }
+    }
+
+    public partial class Weather
+    {
+        public long Id { get; set; }
+        public string Main { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+    }
+
+    public partial class Wind
+    {
+        public double Speed { get; set; }
+        public double Deg { get; set; }
+    }
+
+    //public enum Pod { D, N };
+
+    //public enum Description { BrokenClouds, ClearSky, FewClouds, LightRain, OvercastClouds, ScatteredClouds };
+
+    //public enum MainEnum { Clear, Clouds, Rain };
+}
---
+++ web_api_blazor/Program.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Blazor.Hosting;
+
+namespace web_api_blazor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder()
+                .UseBlazorStartup<Startup>();
+    }
+}
